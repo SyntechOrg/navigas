32f5b718eac848edeecdac733c3a82545a6d90c1
Jxni1 <jonramadani303@gmail.com>
2025-11-05 09:53:39 +0100

    folder delete

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,29 @@
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          "media-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://navigas.vercel.app", "http://localhost:3000"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",

14c055e487ceefd1f9f11e16bf8c0519c784b47a
Jxni1 <jonramadani303@gmail.com>
2025-12-19 11:20:46 +0100

    car slider and top kategorien changed

--- frontend/src/components/car/CarCard.jsx
+++ frontend/src/components/car/CarCard.jsx
@@ -9,6 +9,7 @@ export const CarCard = memo(({ car, pricingType = PRICING_TYPE.NORMAL }) => {
   const handleClick = useCallback(() => {
     navigate(`/api/cars/${car.documentId}?pricing=${pricingType}`);
   }, [navigate, car.documentId, pricingType]);
+  console.log("my car " + JSON.stringify(car, null, 2));
 
   const handleImgError = (e) => (e.target.src = "/images/car.png");
 
@@ -20,9 +21,7 @@ export const CarCard = memo(({ car, pricingType = PRICING_TYPE.NORMAL }) => {
       whileHover={{ y: -8 }}
       transition={{ duration: 0.2 }}
     >
-      <div
-        className="bg-[#0A1424] rounded-xl  shadow-2xl overflow-hidden transition hover:shadow-[0_20px_40px_rgba(0,0,0,0.4)]"
-      >
+      <div className="bg-[#0A1424] rounded-xl  shadow-2xl overflow-hidden transition hover:shadow-[0_20px_40px_rgba(0,0,0,0.4)]">
         <div className="relative overflow-hidden">
           <motion.img
             src={car.imageUrl}
@@ -34,9 +33,7 @@ export const CarCard = memo(({ car, pricingType = PRICING_TYPE.NORMAL }) => {
             transition={{ duration: 0.4, ease: "easeOut" }}
           />
         </div>
-        <div
-          className="py-5 border-b border-[#152032] w-[90%] mx-auto flex flex-col gap-2"
-        >
+        <div className="py-5 border-b border-[#152032] w-[90%] mx-auto flex flex-col gap-2">
           <div className="flex flex-row items-center gap-2">
             <h1 className="text-white font-medium text-[20px] tracking-[2px]">
               {car.displayPrice + " CHF"}
@@ -53,9 +50,7 @@ export const CarCard = memo(({ car, pricingType = PRICING_TYPE.NORMAL }) => {
           </h1>
           <h1 className="text-[#C0C0C1] text-[14px]">{car.Getriebe}</h1>
         </div>
-        <div
-          className="py-3 w-[80%] mx-auto flex justify-between"
-        >
+        <div className="py-3 w-[80%] mx-auto flex justify-between">
           {[
             { icon: "/images/psIcon.svg", text: `${car.leistung} PS` },
             { icon: "/images/pumpIcon.svg", text: car.Treibstoff },
